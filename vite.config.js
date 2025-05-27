35e88ac77ccb28441dcee61a0c6be7d19e31ff38
Weliton Jose <welitonj.jose@gmail.com>
2025-05-27 14:23:34 -0300

    build para CDN

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default {
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'aggenda.js', // nome fixo
+      },
+    },
+  },
+};
+
+// https://cdn.jsdelivr.net/gh/aggenda-com/cdn@master/dist/aggenda.js
